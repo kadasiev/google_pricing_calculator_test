5e51abe40e0d1f154e24aa3120b29f7202e673fe
David Kadasiev <dkadasiev@gmail.com>
2023-11-08 18:36:56 +0400

    m

--- src/test/java/tests/GooglePricingCalculatorTest.java
+++ src/test/java/tests/GooglePricingCalculatorTest.java
@@ -29,7 +29,7 @@ public class GooglePricingCalculatorTest extends BaseTest{
                 .openYOPMailBox()
                 .getEstimateMonthlyCostFromEmail();
 
-        Assert.assertEquals(estimateMonthlyCostFromEmail,
+        Assert.assertEquals(estimateMonthlyCostFromEmail + 1,
                 virtualMachine.getPrice(),
                 "Verification of the estimate price sent by email from Google calculator");
     }
